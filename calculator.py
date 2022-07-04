3580c006982ceeabbedca176e161abdd0f9a26a8
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-07-04 19:59:54 +0300

    my calculator

--- calculator.py
+++ calculator.py
@@ -1,9 +1,39 @@
+def calculator(myFirstNumber, mySecondNumber, myOpration) :
+
+	if myOpration == '+' or myOpration =='-' or myOpration == '*' or myOpration == '/':
+		if myOpration == '+' :
+			myResoult = myFirstNumber + mySecondNumber
+			print("the results is :" +str(myResoult))
+		elif myOpration == '-' :
+			myResoult = myFirstNumber - mySecondNumber
+			print("the results is :" +str(myResoult))
+		elif myOpration == '*' :
+			myResoult = myFirstNumber * mySecondNumber
+			print("the results is :" +str(myResoult))
+		elif myOpration == '/' :
+			myResoult = myFirstNumber / mySecondNumber
+			print("the results is :" +str(myResoult))
+
+	else : print("your Opration is invalid")
+		
 
 def main():
 	#write your code here
-	pass
-	
+	#pass
+	firstNumber = input("Enter the first number:")
+	secondNumber = input("Enter the second number:")
+	operation = input("Choose the operation (+, -, /, *):")
 
+	print(type(firstNumber))
+
+	print(type(secondNumber))
+ 
+	if firstNumber.isdigit() and secondNumber.isdigit():
+		calculator(int(firstNumber), int(secondNumber), operation)
+	else:
+		print("your numbers is invalid")
+
+ 
 
 
 if __name__ == '__main__':
